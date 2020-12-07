a92f2b7a680829443b85cb01b8b71ba4e3740806
Aaron Gertler <aaron.gertler@gmail.com>
2020-12-07 10:22:21 -0600

    rendering Firebase db to DOM

--- src/components/List.js
+++ src/components/List.js
@@ -1,10 +1,39 @@
 import React, { useEffect, useState } from 'react'
 import { getAll } from '../modules/APICalls'
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
 
 export const List = () => {
+    const [listArray, setListArray] = useState([])
+
+    useEffect(() => {
+        getAll()
+            .then(data => {
+                let newArray = Object.keys(data).map((key, index) => {
+                    data[key].fbid = key
+                    return data[key]
+                })
+                setListArray(newArray)
+            })
+    }, [])
+
     return (
         <>
-            <h1>Christmas List</h1>
+            <h4>Christmas List</h4>
+            <Container fluid>
+                <Row>
+                    {
+                        listArray.map(item => {
+                            return (
+                                <Col key={item.fbid} className='bgcolor' xs={12} md={6} lg={4} xl={2}>
+                                    <h5>{item.title}</h5>
+                                </Col>
+                            )
+                        })
+                    }
+                </Row>
+            </Container>
         </>
     )
 }
